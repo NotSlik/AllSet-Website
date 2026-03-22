f07fb19fcde5c2e2a1b8eed5e117cf3516815612
NotSlik <laithala52627@gmail.com>
2026-03-22 18:18:00 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const sections = document.querySelectorAll('.section');
-window.addEventListener('scroll', () => {
-  sections.forEach(s => {
-    if (s.getBoundingClientRect().top < window.innerHeight*0.85) {
-      s.classList.add('visible');
-    }
-  });
-});
